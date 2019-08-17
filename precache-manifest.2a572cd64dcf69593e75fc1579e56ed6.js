7fca573d82e9cc701b269cd9d5379947da8ba1a4
Carlos Eduardo <carloseduardo.oceano@gmail.com>
2019-08-17 00:18:06 -0300

    Updates

---
+++ precache-manifest.2a572cd64dcf69593e75fc1579e56ed6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16b0c2991f7b74c4d482f08785ac00fb",
+    "url": "/my-series-app/index.html"
+  },
+  {
+    "revision": "c8711b271da1e295c80c",
+    "url": "/my-series-app/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "c8711b271da1e295c80c",
+    "url": "/my-series-app/static/js/2.e610f046.chunk.js"
+  },
+  {
+    "revision": "b4ed608f0146f4acdf68",
+    "url": "/my-series-app/static/js/main.50cebf6a.chunk.js"
+  },
+  {
+    "revision": "ccb12690f8296262391c",
+    "url": "/my-series-app/static/js/runtime~main.2afa6974.js"
+  }
+]);
